3179dccdac4e5fa833d2e9a4c47da24e7f260958
NaseefNizar <naseefnizar11@gmail.com>
2023-11-01 13:42:04 +0530

    after hosting 2

--- server/src/types/environment.d.ts
+++ server/src/types/environment.d.ts
@@ -5,8 +5,15 @@ declare global {
     interface ProcessEnv {
       JWT_SECRET_KEY: string;
       DB_URL: string;
+      TWILIO_ACCOUNT_SID: string;
+      TWILIO_AUTH_TOKEN: string;
+      TWILIO_SERVICE_SID: string;
+
+      CLOUD_NAME: string;
+      API_KEY: string;
+      API_SECRET: string;
+
+      STRIPE_SECRET_KEY: string;
     }
   }
 }
-
-
