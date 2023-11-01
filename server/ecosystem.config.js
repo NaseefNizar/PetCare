6149109fc9531ac7361a297a17a84ebd7a3c339e
NaseefNizar <naseefnizar11@gmail.com>
2023-11-01 15:18:41 +0530

    twilio err2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,23 @@
 module.exports = {
+    // apps: [
+    //   {
+    //     name: 'my-app',
+    //     script: 'node_modules/.bin/concurrently',
+    //     args: [
+    //       'node_modules/.bin/tsc -w',  // Watch TypeScript files
+    //       'node_modules/.bin/nodemon dist/index.js',  // Run the compiled app.js
+    //     ],
+    //     watch: true,  // Enable automatic restarts on file changes
+    //     interpreter: 'none',  // Avoid interpreting your command
+    //   },
+    // ],
     apps: [
-      {
-        name: 'my-app',
-        script: 'node_modules/.bin/concurrently',
-        args: [
-          'node_modules/.bin/tsc -w',  // Watch TypeScript files
-          'node_modules/.bin/nodemon dist/index.js',  // Run the compiled app.js
-        ],
-        watch: true,  // Enable automatic restarts on file changes
-        interpreter: 'none',  // Avoid interpreting your command
-      },
-    ],
+        {
+          name: 'my-app',
+          script: 'npm',
+          args: 'start',
+          watch: true,  // Enable automatic restarts on file changes
+          interpreter: 'none',  // Avoid interpreting your command
+        },
+      ],
   };
