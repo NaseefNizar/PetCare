1a6cb4ba6ec52aeb609fc9bd64676bc4956d8729
NaseefNizar <naseefnizar11@gmail.com>
2023-11-01 14:59:38 +0530

    twilio err2

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,3 +1,11 @@
+// ecosystem.config.cjs
+const { createRequire } = require('module');
+const require = createRequire(import.meta.url);
+
+// Import the ESM configuration file using dynamic import
+const esmConfig = await import('./ecosystem.config.js');
+
+module.exports = esmConfig.default;
 module.exports = {
     apps: [
       {
---
+++ server/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: 'my-app',
+        script: 'node_modules/.bin/concurrently',
+        args: [
+          'node_modules/.bin/tsc -w',  // Watch TypeScript files
+          'node_modules/.bin/nodemon dist/index.js',  // Run the compiled app.js
+        ],
+        watch: true,  // Enable automatic restarts on file changes
+        interpreter: 'none',  // Avoid interpreting your command
+      },
+    ],
+  };
