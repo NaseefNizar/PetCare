17de82558cfdf1b93fcb75d0ff1e83ba259304db
NaseefNizar <naseefnizar11@gmail.com>
2023-11-01 14:47:47 +0530

    twilio err2

--- server/ecosystem.config.mjs
+++ server/ecosystem.config.mjs
@@ -5,7 +5,7 @@ module.exports = {
         script: 'node_modules/.bin/concurrently',
         args: [
           'node_modules/.bin/tsc -w',  // Watch TypeScript files
-          'node_modules/.bin/nodemon dist/app.js',  // Run the compiled app.js
+          'node_modules/.bin/nodemon dist/index.js',  // Run the compiled app.js
         ],
         watch: true,  // Enable automatic restarts on file changes
         interpreter: 'none',  // Avoid interpreting your command
