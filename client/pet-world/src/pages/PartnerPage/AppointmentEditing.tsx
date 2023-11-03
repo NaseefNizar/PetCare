767b1156806d230fdba7ae47c48300bda368f0ea
NaseefNizar <naseefnizar11@gmail.com>
2023-11-03 13:40:26 +0530

    after build

--- client/pet-world/src/pages/PartnerPage/AppointmentEditing.tsx
+++ client/pet-world/src/pages/PartnerPage/AppointmentEditing.tsx
@@ -1,4 +1,4 @@
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
